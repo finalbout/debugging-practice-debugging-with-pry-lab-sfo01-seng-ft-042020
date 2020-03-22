d3a40fe8e34b0659d6abd98eea07f45b0d1d35d3
John Tom Jr <johntjr.22@gmail.com>
2020-03-22 03:19:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -7,6 +7,7 @@ def turtles
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
+binding.pry
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
@@ -15,4 +16,3 @@ def turtle_traits(turtles)
     end
   end
 end
-binding.pry
